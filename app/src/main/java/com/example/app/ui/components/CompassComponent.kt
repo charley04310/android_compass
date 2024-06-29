a0bfd2801997bc10c49e48fdb2357861c32623b8
Mauran <thomas.mauran@etu.umontpellier.fr>
2024-06-29 19:14:52 +0200

    feat: improve point
    Signed-off-by: Mauran <thomas.mauran@etu.umontpellier.fr>

--- app/src/main/java/com/example/app/ui/components/CompassComponent.kt
+++ app/src/main/java/com/example/app/ui/components/CompassComponent.kt
@@ -1,9 +1,11 @@
 package com.example.app.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
@@ -16,23 +18,33 @@ fun CompassComponent(
     modifier: Modifier = Modifier,
     azimuth: Float
 ) {
+    val compassImage = if (false) R.drawable.boussole else R.drawable.point
+    val backgroundImage = if (true) R.drawable.arrow else null
 
-    // change the image if no point is selected
-    val image: Int;
+    Box(
+        modifier = modifier.size(500.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        backgroundImage?.let {
+            Image(
+                painter = painterResource(id = it),
+                contentDescription = "Background Image",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .rotate(-90.0F)
+                    .align(Alignment.Center)
+            )
+        }
 
-    if (false) {
-        image = R.drawable.boussole
-    } else {
-        image = R.drawable.point
+        Image(
+            painter = painterResource(id = compassImage),
+            contentDescription = "Compass Image",
+            modifier = Modifier
+                .size(500.dp)
+                .clip(CircleShape)
+                .rotate(azimuth)
+                .align(Alignment.Center)
+        )
     }
-    Image(
-        modifier = modifier
-            .size(500.dp)
-            .clip(CircleShape)
-            .rotate(azimuth),
-        painter = painterResource(id = image),
-        contentDescription = "Compass Image",
-    )
 }
-
-
